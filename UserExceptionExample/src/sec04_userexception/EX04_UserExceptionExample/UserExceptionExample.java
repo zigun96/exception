d91d65cf3191ede594392f37c66b3e7646a6b6a5
jaegeon cha <jaegun920@naver.com>
2022-09-29 11:09:33 +0900

    Exception example

--- UserExceptionExample/src/sec04_userexception/EX04_UserExceptionExample/UserExceptionExample.java
+++ UserExceptionExample/src/sec04_userexception/EX04_UserExceptionExample/UserExceptionExample.java
@@ -10,6 +10,36 @@ class MinusException extends Exception {
 	}
 }
 // 점수가 100점을 초과할 때 예외 발생
+class OverException extends Exception {
+	public OverException() {
+		super();
+	}
+	public OverException(String message) {
+		super(message);
+	}
+}
+
+class A {
+	void checkScore(int score) throws MinusException, OverException {
+		if(score < 0) {
+			throw new MinusException("예외 발생: 음숫값 입력");
+		}
+		else if (score > 100)
+			throw new OverException("예외 발생: 100점 초과");
+		else
+			System.out.println("정상적인 값입니다.");
+	}
+}
+
 public class UserExceptionExample {
-	
+	public static void main(String[] args) {
+		
+		A a = new A();
+		try {
+			a.checkScore(85);
+			a.checkScore(150);
+		} catch (MinusException | OverException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
